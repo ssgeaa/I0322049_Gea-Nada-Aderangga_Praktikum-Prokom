9668d024b34df8fd714966f6debf4dc907c95d9f
ssgeaa <aderangga@student.uns.ac.id>
2023-03-15 13:40:19 +0700

    menambahkan file Exercise-01.py

---
+++ Exercise-01.py
@@ -0,0 +1,19 @@
+print("Nama: Gea Nada Aderangga")
+print("NIM: I0322049")
+print("Tempat tinggal: Kalinegoro, Kabupaten Magelang")
+
+print("")
+
+print("Saya bertempat tinggal di suatu desa yang cukup ramai dihuni para penduduk dengan segala keramaiannya. Desa tempat saya tinggal terletak di lokasi yang cukup jauh dari kota, butuh waktu sekitar 10-20 menit untuk bisa sampai ke sana. Namun, lokasi yang jauh itu tidak membuat desa saya lantas menjadi desa yang terpencil ataupun tertinggal, karena pertumbuhannya bisa dibilang cukup pesat beberapa tahun terakhir.")
+
+print("")
+
+print("Dulunya, di desa saya banyak sekali lahan hijau di pinggir jalan. Seiring berjalannya waktu, lahan hijau tersebut berkurang karena dimanfaatkan untuk lahan pemukiman ataupun kawasan pertokoan dan kantor. Pertumbuhan pembangunan itu diiringi dengan bertumbuhnya aktivitas ekonomi dan sosial masyarakat di desa saya. Semakin banyak masyarakat yang membangun suatu usaha sebagai upaya untuk menaikkan taraf hidup mereka. Ditambah lagi, semakin banyak pula kegiatan-kegiatan sosial yang diadakan di masyarakat seperti jalan sehat, senam bersama, maupun bazaar makanan yang diadakan secara berkala.")
+
+print("")
+
+print("Jalan utama yang berada di desa tempat saya tinggal juga berfungsi sebagai jalan alternatif menuju Candi Borobudur, yang merupakan tempat wisata favorit para pelancong saat berkunjung ke Magelang. Karena itulah, banyak warga setempat yang pandai memanfaatkan situasi tersebut untuk menjadikan para pendatang sebagai sasaran bisnisnya, misalnya membangun rumah makan, cafe, dan sebagainya.")
+
+print ("")
+
+print ("Harapan saya saat nantinya saya lulus dari Teknik Industri UNS adalah saya bisa menerapkan ilmu-ilmu yang sudah saya dapat dengan baik, entah pada kehidupan sehari-hari maupun pada dunia pekerjaan. Saya berharap pula, apa yang telah saya dapat di sini dapat membantu saya untuk mencapai apapun yang saya mau di kehidupan saya.")
